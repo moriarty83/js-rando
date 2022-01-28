c2895c6cbc936553dd147171939438378a2577a0
moriarty83 <moriarty83@gmail.com>
2022-01-28 15:35:39 -0500

    made class

--- js-rando.js
+++ js-rando.js
@@ -1,43 +1,48 @@
 // Creates Random Float between 0 & 1 (including 0, not including 1 with 16 significant digits)
-function Random(digits = 16){
-    digits = digits > 16 ? 16 : digits
-    return Math.random().toFixed(digits);
-}
 
-// Returns a random integrer from min (inclusive) to max (exclusive)
-function RandomInt(min, max){
-    return Math.floor(Math.random() * ((max) - min) + min);
-}
+class Rando{
 
+    constructor(){}
 
-function RandomFloat(min, max, digits=16){
-    digits = digits > 16 ? 16 : digits
-    return (Math.floor(Math.random() * ((max) - min) + min)+Random()).toFixed(digits);
-}
-
-function RandomElement(array){
-    return array[RandomInt(0, array.length)]
-}
-
-function RandomHex(){
-    return RandomInt(0, 16).toString(16)
-}
-
-function RandomHexColor(){
-    hex = "#"
-    for(let i = 0; i < 6; i++){
-        hex = hex + RandomHex()
+    Random (digits = 16){
+        digits = digits > 16 ? 16 : digits
+        return Math.random().toFixed(digits);
     }
-    return hex
+
+    // Returns a random integrer from min (inclusive) to max (exclusive)
+    RandomInt = function(min, max){
+        return Math.floor(Math.random() * ((max) - min) + min);
+    }
+
+    RandomFloat(min, max, digits=16){
+        digits = digits > 16 ? 16 : digits
+        return (Math.floor(Math.random() * ((max) - min) + min)+this.Random()).toFixed(digits);
+    }
+
+    RandomElement(array){
+        return array[this.RandomInt(0, array.length)]
+    }
+
+    RandomHex(){
+        return this.RandomInt(0, 16).toString(16)
+    }
+
+    RandomHexColor(){
+        let hex = "#"
+        for(let i = 0; i < 6; i++){
+            hex = hex + this.RandomHex()
+        }
+        return hex
+    }
+
+    RandomRGBColor(){
+        return `(${this.RandomInt(0,256)}, ${this.RandomInt(0,256)}, ${this.RandomInt(0,256)})`
+    }
+
+    RandomRGBAColor(){
+        return `(${this.RandomInt(0,256)}, ${this.RandomInt(0,256)}, ${this.RandomInt(0,256)}, ${Number.parseFloat(this.Random()).toPrecision(1)})`
+    }
+
 }
+export default Rando
 
-function RandomRGBColor(){
-    return `(${RandomInt(0,256)}, ${RandomInt(0,256)}, ${RandomInt(0,256)})`
-}
-
-function RandomRGBAColor(){
-    return `(${RandomInt(0,256)}, ${RandomInt(0,256)}, ${RandomInt(0,256)}, ${Number.parseFloat(Random()).toPrecision(1)})`
-}
-
-
-console.log (RandomRGBAColor())
---
+++ readme.md
@@ -0,0 +1,3 @@
+# JS-Rando
+
+A simple library of functions to generate random integers, floats, array elements, and colors.
